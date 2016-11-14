a6c98faf791b8e90ffc805ed13ab97705aab7638
Carlos Muñoz-Romero <carlos.munoz@labyrinz.es>
2016-11-14 10:13:45 +0100

    New version for rtve

---
+++ js/config.js
@@ -0,0 +1,15 @@
+var config = {
+  tv3: {
+    videos: 'http://mp4-high-dwn.media.tv3.cat/g/tvcatalunya/'
+  },
+  tve: {
+    videos: 'http://origin-proyectos-lab-externos.rtve.es/webdocs/xavier-cugat/videos/'
+  },
+  local: {
+    videos: location.pathname+'videos/'
+  },
+  doc: {
+    link: 0, // 0 = off, 1 = on
+    url: "http://www.ccma.cat/tv3/documentals/xavier-cugat/"
+  }
+}
